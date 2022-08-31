75e11121f60d44fb276460d20f542999186afc3a
Castro-X <110267145+Castro-X@users.noreply.github.com>
2022-08-31 22:34:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+
+let employee = {
+    name: 'Castro',
+    streetAddress : '14th street' 
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+     const newEmployee = {...employee};
+
+     newEmployee[key] = value;
+      return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+
+    employee.name = 'Bob';
+    employee.streetAddress = '49th Helm';
+
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+
+    delete newEmployee.name;
+
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+    delete employee.name;
+    return employee;
+}
